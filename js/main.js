3954fcaf9aeca755f6a6e4396bd0dbd6510d9c5a
syed naqi <shahnacirema@gmail.com>
2021-04-14 18:25:04 +0500

    responsive 576

--- js/main.js
+++ js/main.js
@@ -3,7 +3,19 @@ jQuery(document).ready(function($){
         slidesToShow:5,
         nav:true,
         prevArrow:"<button class='slick-prev'><i class='fa fa-arrow-left'></i></button>",
-        nextArrow:"<button class='slick-next'><i class='fa fa-arrow-right'></i></button>"
+        nextArrow:"<button class='slick-next'><i class='fa fa-arrow-right'></i></button>",
+        responsive: [
+            {
+              breakpoint: 501,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: false,
+              }
+            },
+
+        ]
 
     })
     
@@ -12,7 +24,18 @@ jQuery(document).ready(function($){
         slidesToShow:3,
         nav:true,
         prevArrow:"<button class='slick-prev'><i class='fa fa-arrow-left'></i></button>",
-        nextArrow:"<button class='slick-next'><i class='fa fa-arrow-right'></i></button>"
+        nextArrow:"<button class='slick-next'><i class='fa fa-arrow-right'></i></button>",
+        responsive: [
+            {
+              breakpoint: 501,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: false,
+              }
+            },
 
+        ]
     })
 })
