eb10437d4dbb92161e6137aa8724ff68f26b361b
syed naqi <shahnacirema@gmail.com>
2021-04-14 15:39:37 +0500

    changes 3:30

--- js/main.js
+++ js/main.js
@@ -6,4 +6,13 @@ jQuery(document).ready(function($){
         nextArrow:"<button class='slick-next'><i class='fa fa-arrow-right'></i></button>"
 
     })
+    
+    
+    $(".team-members").slick({
+        slidesToShow:3,
+        nav:true,
+        prevArrow:"<button class='slick-prev'><i class='fa fa-arrow-left'></i></button>",
+        nextArrow:"<button class='slick-next'><i class='fa fa-arrow-right'></i></button>"
+
+    })
 })
